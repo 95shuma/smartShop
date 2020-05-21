b8472038090d47fd1cc440c249a0ebaa1563a169
95shuma <95shuma@gmail.com>
2020-05-21 22:47:59 +0600

    hw-71 some-change in basket

--- src/main/java/com/shop/smart/config/LocaleConfig.java
+++ src/main/java/com/shop/smart/config/LocaleConfig.java
@@ -1,5 +1,8 @@
 package com.shop.smart.config;
 
+import com.shop.smart.model.User;
+import com.shop.smart.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
@@ -8,8 +11,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
+import java.security.Principal;
+
 @Configuration
 public class LocaleConfig implements WebMvcConfigurer {
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(localeChangeInterceptor());
